e24ec580ccdee685b920259345963b652d29a9f7
Hana Becha <bechahana@gmail.com>
2023-11-27 14:46:20 +0100

    working

---
+++ src/App.js
@@ -0,0 +1,130 @@
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
+// 3. Setup webcam and canvas DONE
+// 4. Define references to those DONE
+// 5. Load posenet DONE
+// 6. Detect function DONE
+// 7. Drawing utilities from tensorflow DONE
+// 8. Draw functions DONE
+import { drawPoint } from "./utilities";
+
+import React, { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+    //
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+  
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.7, ctx);
+  
+    // Find the keypoints for the shoulders
+    const leftShoulder = pose["keypoints"].find((point) => point.part === "leftShoulder");
+    const rightShoulder = pose["keypoints"].find((point) => point.part === "rightShoulder");
+  
+    // Find the keypoints for the hips
+    const leftHip = pose["keypoints"].find((point) => point.part === "leftHip");
+    const rightHip = pose["keypoints"].find((point) => point.part === "rightHip");
+  
+    // Draw a line to represent the sternum bone
+    if (leftShoulder && rightShoulder && leftHip && rightHip) {
+      const sternumX = (leftShoulder.position.x + rightShoulder.position.x + leftHip.position.x + rightHip.position.x) / 4;
+      const sternumY = (leftShoulder.position.y + rightShoulder.position.y + leftHip.position.y + rightHip.position.y) / 4;
+  
+      ctx.beginPath();
+      ctx.moveTo((leftShoulder.position.x + rightShoulder.position.x) / 2, (leftShoulder.position.y + rightShoulder.position.y) / 2);
+      ctx.lineTo(sternumX, sternumY);
+      ctx.lineTo((leftHip.position.x + rightHip.position.x) / 2, (leftHip.position.y + rightHip.position.y) / 2);
+  
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "red";
+      ctx.stroke();
+    }
+  };
+  
+  
+  
+  runPosenet();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
